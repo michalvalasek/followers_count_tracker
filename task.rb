fdd2eae2cb36fd73c87ab06a67b0940fba3b21fa
root <root@Mnemosyne.(none)>
2012-03-08 22:10:43 +0100

    removed growl notifications, fixed bugs

--- task.rb
+++ task.rb
@@ -6,21 +6,23 @@
 # 2012
 
 require 'twitter'
-require 'growl'
 
-f_usernames = File.open('usernames.txt')
+app_dir = File.dirname(__FILE__) 
+
+f_usernames = File.open(app_dir + '/usernames.txt')
 
 processed_usernames = 0
-
 f_usernames.each do |line|
   
   username = line.match(/^[a-zA-Z_]*/).to_s
   next if username.empty? # skip line if incorrect username
   
-  user_file = File.open("storage/#{username}.txt",'a+')
+  user_file = File.open(app_dir + "/storage/#{username}.txt",'a+')
+  user_file.rewind
   
   # skip loop if we already have line for today in the output file
-  next if user_file.size>0 && user_file.lines.to_a.last[0..9]==Time.now.strftime('%Y-%m-%d')
+  lines = user_file.lines.to_a
+  next if lines.last && lines.last[0..9]==Time.now.strftime('%Y-%m-%d')
   
   begin
     user = Twitter.user(username)
@@ -40,5 +42,3 @@ f_usernames.close
 
 end_message = "Usernames processed: #{processed_usernames}"
 puts end_message
-
-Growl.notify end_message, title: "Followers Count Tracker"
